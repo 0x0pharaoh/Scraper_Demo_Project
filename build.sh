30be77a4bdcc83635508b0e9771485605174c651
Ritika <sharitika9116@gmail.com>
2025-08-11 11:26:19 +0530

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
 echo "Installing Playwright Chromium..."
-playwright install chromium
+python -m playwright install chromium
+
 
 
 
